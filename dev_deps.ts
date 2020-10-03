bf45b446fb4e915089824bb66f7d47c97cdde6e8
FaberVitale <faber.vitale@gmail.com>
2020-10-03 15:54:55 +0200

    chore(deps): update to std@0.73.0 & ci to latest

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,5 +2,5 @@ export {
   assertEquals,
   assertThrowsAsync,
 } from "https://deno.land/std/testing/asserts.ts";
-export * as path from "https://deno.land/std@0.70.0/path/mod.ts";
-export * as io from "https://deno.land/std@0.70.0/io/mod.ts";
+export * as path from "https://deno.land/std@0.73.0/path/mod.ts";
+export * as io from "https://deno.land/std@0.73.0/io/mod.ts";
