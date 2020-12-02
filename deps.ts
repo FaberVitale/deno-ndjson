37a8dff407df5d800258e8d164fb0956c4ababcf
FaberVitale <faber.vitale@gmail.com>
2020-12-02 19:04:31 +0100

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { readLines } from "https://deno.land/std@0.73.0/io/bufio.ts";
-export { encode as encodeToUTF8 } from "https://deno.land/std@0.73.0/encoding/utf8.ts";
+export { readLines } from "https://deno.land/std@0.79.0/io/bufio.ts";
+export { encode as encodeToUTF8 } from "https://deno.land/std@0.79.0/encoding/utf8.ts";
