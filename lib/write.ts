06123e9a1038243d4bb247b756cb0b88442f2553
FaberVitale <faber.vitale@gmail.com>
2020-12-07 10:29:40 +0100

    docs: update import version

--- lib/write.ts
+++ lib/write.ts
@@ -11,7 +11,7 @@ import { encodeToUTF8 } from "../deps.ts";
  * **Usage**
  * 
  * ```typescript
- * import { writeNdjson } from 'https://deno.land/x/ndjson@v1.0.4/mod.ts';
+ * import { writeNdjson } from 'https://deno.land/x/ndjson@v1.0.5/mod.ts';
  * 
  * const toBeWritten = [
  *   { message: 'qui', level: 'info', timestamp: '2020-05-08T14:05:25.091Z' }, 
